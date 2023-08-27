8b473a09c06e8bc9605eba27855719d5bc77f9ce
yepdev <yep.dev7@gmail.com>
2023-08-27 12:55:39 +0200

    Add deploy script

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import viteTsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
+  base: '/local/dashboard/'
 })
