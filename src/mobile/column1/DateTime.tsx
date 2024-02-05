10c7aa2f9801c860f6e0c97fadd6906fe8f16191
yepdev <yep.dev7@gmail.com>
2024-02-05 17:31:17 +0100

    Fix time offset

--- src/mobile/column1/DateTime.tsx
+++ src/mobile/column1/DateTime.tsx
@@ -1,36 +1,33 @@
 import { Card, Stack, Txt } from '@components'
 import { useEntity } from '@hakit/core'
 import { format } from 'date-fns'
-import { utcToZonedTime } from 'date-fns-tz'
+import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz'
 
 export const DateTime = () => {
   const timeEntity = useEntity('sensor.time')
   const dateEntity = useEntity('sensor.date')
 
-  const dateTimeString = `${dateEntity.state}T${timeEntity.state}:00Z`
-
+  const dateTimeString = `${dateEntity.state}T${timeEntity.state}:00`
   const timeZoneWarsaw = 'Europe/Warsaw'
-  const timeZoneLosAngeles = 'America/Los_Angeles'
-  const timeZoneEastern = 'America/New_York' // Eastern Time Zone
 
   // todo: upgrade to date-fns 3.0 once -tz supports it and cleanup ignores
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+  const dateTimeInWarsaw = zonedTimeToUtc(dateTimeString, timeZoneWarsaw)
+  // @ts-expect-error
+  const dateStringWarsaw = format(dateTimeInWarsaw, 'E d MMM', { timeZone: timeZoneWarsaw })
+
+  const timeZoneLosAngeles = 'America/Los_Angeles'
+  const timeZoneEastern = 'America/New_York'
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const zonedDateWarsaw = utcToZonedTime(dateTimeString, timeZoneWarsaw)
-  // @ts-expect-error
-  const timeStringWarsaw = format(zonedDateWarsaw, 'HH:mm', { timeZone: timeZoneWarsaw })
-  // @ts-expect-error
-  const dateStringWarsaw = format(zonedDateWarsaw, 'E d MMM', { timeZone: timeZoneWarsaw })
-
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const zonedDateLosAngeles = utcToZonedTime(dateTimeString, timeZoneLosAngeles)
+  const zonedDateLosAngeles = utcToZonedTime(dateTimeInWarsaw, timeZoneLosAngeles)
   const timeStringLosAngeles = format(zonedDateLosAngeles, 'hh:mm a', {
     // @ts-expect-error
     timeZone: timeZoneLosAngeles,
   })
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const zonedDateEastern = utcToZonedTime(new Date(dateTimeString), timeZoneEastern)
+  const zonedDateEastern = utcToZonedTime(dateTimeInWarsaw, timeZoneEastern)
   // @ts-expect-error
   const timeStringEastern = format(zonedDateEastern, 'hh:mm a', { timeZone: timeZoneEastern })
 
@@ -41,7 +38,7 @@ export const DateTime = () => {
       </Stack>
       <Stack alignItems="center">
         <Txt weight="bold" size="xl" style={{ marginRight: 16 }}>
-          {timeStringWarsaw}
+          {timeEntity.state}
         </Txt>
         <Stack column gap={0}>
           <Txt size="sm"> {timeStringEastern}</Txt>
