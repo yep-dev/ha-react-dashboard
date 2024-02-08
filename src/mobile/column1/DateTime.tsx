822e11da75d8c7458b894809def23511e931b4aa
yepdev <yep.dev7@gmail.com>
2024-02-08 12:17:49 +0100

    Migrate from HA-based to browser time

--- src/mobile/column1/DateTime.tsx
+++ src/mobile/column1/DateTime.tsx
@@ -1,44 +1,33 @@
 import { Card, Stack, Txt } from '@components'
-import { useEntity } from '@hakit/core'
 import { format } from 'date-fns'
-import { utcToZonedTime, zonedTimeToUtc } from 'date-fns-tz'
+import { utcToZonedTime } from 'date-fns-tz'
 
 export const DateTime = () => {
-  const timeEntity = useEntity('sensor.time')
-  const dateEntity = useEntity('sensor.date')
-
-  const dateTimeString = `${dateEntity.state}T${timeEntity.state}:00`
-  const timeZoneWarsaw = 'Europe/Warsaw'
+  const browserDateTime = new Date()
 
   // todo: upgrade to date-fns 3.0 once -tz supports it and cleanup ignores
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const dateTimeInWarsaw = zonedTimeToUtc(dateTimeString, timeZoneWarsaw)
-  // @ts-expect-error
-  const dateStringWarsaw = format(dateTimeInWarsaw, 'E d MMM', { timeZone: timeZoneWarsaw })
-
   const timeZoneLosAngeles = 'America/Los_Angeles'
-  const timeZoneEastern = 'America/New_York'
-
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const zonedDateLosAngeles = utcToZonedTime(dateTimeInWarsaw, timeZoneLosAngeles)
+  const zonedDateLosAngeles = utcToZonedTime(browserDateTime, timeZoneLosAngeles)
   const timeStringLosAngeles = format(zonedDateLosAngeles, 'hh:mm a', {
     // @ts-expect-error
     timeZone: timeZoneLosAngeles,
   })
 
+  const timeZoneEastern = 'America/New_York'
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  const zonedDateEastern = utcToZonedTime(dateTimeInWarsaw, timeZoneEastern)
+  const zonedDateEastern = utcToZonedTime(browserDateTime, timeZoneEastern)
   // @ts-expect-error
   const timeStringEastern = format(zonedDateEastern, 'hh:mm a', { timeZone: timeZoneEastern })
 
   return (
     <Card align="space-around">
       <Stack>
-        <Txt size="xl">{dateStringWarsaw}</Txt>
+        <Txt size="xl">{format(browserDateTime, 'E d MMM')}</Txt>
       </Stack>
       <Stack alignItems="center">
         <Txt weight="bold" size="xl" style={{ marginRight: 16 }}>
-          {timeEntity.state}
+          {format(browserDateTime, 'HH:mm')}
         </Txt>
         <Stack column gap={0}>
           <Txt size="sm"> {timeStringEastern}</Txt>
