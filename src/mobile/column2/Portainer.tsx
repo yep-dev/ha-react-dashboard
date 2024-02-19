5e534086e76e8e324c04ee68a01f0cd11d725f15
yepdev <yep.dev7@gmail.com>
2024-02-19 03:31:14 +0100

    Add broken portainer handling

--- src/mobile/column2/Portainer.tsx
+++ src/mobile/column2/Portainer.tsx
@@ -6,33 +6,41 @@ import { useInterval } from 'react-use'
 type Response = { State: string; Labels: Record<string, string> }[]
 
 export const Portainer = () => {
-  const [data, setData] = useState<Response | null>(null)
+  type TransformedData = { content: string }[]
+
+  const [data, setData] = useState<TransformedData | null>(null)
 
   const fetchData = async () => {
     try {
-      const response = await ky.get('http://10.0.0.100:9441/containers').json()
-      setData(response as Response)
+      const response = await ky.get('http://10.0.0.100:9441/containers').json<Response>()
+      const transformedData = response
+        .filter(
+          (entity) =>
+            entity.State !== 'running' &&
+            !['web-signifier'].includes(entity.Labels['com.docker.compose.project']),
+        )
+        .map((entity) => ({
+          content: `${entity.Labels['com.docker.compose.service']} ${entity.State}`,
+        }))
+      setData(transformedData)
     } catch (error) {
-      console.error('Failed to fetch data:', error)
+      setData([{ content: "Couldn't fetch portainer data" }])
     }
   }
 
   useEffect(() => {
     fetchData()
   }, [])
+
   useInterval(fetchData, 10000)
 
-  return data
-    ? data
-        .filter(
-          (entity) =>
-            entity.State != 'running' &&
-            !['web-signifier'].includes(entity.Labels['com.docker.compose.project']),
-        )
-        .map((entity, i) => (
-          <Card.Padded key={i} align="flex-start">
-            {entity.Labels['com.docker.compose.service']} {entity.State}
-          </Card.Padded>
-        ))
-    : null
+  return (
+    <>
+      {data?.map((item) => (
+        <Card.Padded key={item.content} align="flex-start">
+          {item.content}
+        </Card.Padded>
+      ))}
+    </>
+  )
 }
